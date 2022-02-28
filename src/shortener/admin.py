b5928814edbb475592933a6ea0e2364de912419d
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-03-01 00:12:53 +0100

    feat: register Shortener to admin

--- src/shortener/admin.py
+++ src/shortener/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Shortener
 
 # Register your models here.
+class ShortenerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Shortener, ShortenerAdmin)
