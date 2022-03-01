d815c138c8e4a79d81a2ca3b3da97296db1d2b8a
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-03-01 02:17:25 +0100

    refactor: switch places of db attrs

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Shortener(models.Model):
-    original = models.URLField(max_length=400)
     shortened = models.SlugField(max_length=50)
+    original = models.URLField(max_length=400)
