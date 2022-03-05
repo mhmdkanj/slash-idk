76144affab1346ad0e8c8d67d116e6a5092f02c9
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-03-05 21:32:26 +0100

    refactor: move suggest name button next to shortened url field

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -15,7 +15,7 @@ class ShortenerForm(forms.ModelForm):
         self.helper.form_method = 'POST'
         self.helper.layout = Layout(
             Row(
-                Column('original', css_class='form-group col-lg-5'),
+                Column('original', css_class='form-group col-lg-6'),
                 css_class='form-row justify-content-center align-items-center mt-5'
             ),
             Row(
@@ -24,10 +24,11 @@ class ShortenerForm(forms.ModelForm):
             ),
             Row(
                 Column(PrependedText('shortened', "http://localhost:8000/"), css_class='form-group col-lg-3'),
-                css_class='form-row justify-content-center align-items-center mt-3'
+                Column(HTML('<a class="btn btn-secondary" href="/suggest/name/" role="button">Suggest Name</a>'), css_class='form-group col-lg-2'),
+                css_class='form-row justify-content-center mt-3'
             ),
             Row(
-                Column(Submit('submit', '     Save     '), css_class='form-group col-lg-3 d-flex justify-content-center'),
+                Column(Submit('submit', '    Save    '), css_class='form-group col-lg-1 d-flex justify-content-center'),
                 css_class='form-row justify-content-center align-items-center mt-3'
             )    
         )
