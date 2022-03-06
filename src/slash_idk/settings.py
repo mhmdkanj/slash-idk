3d643bcaa16ff57c810e36975967e06343e88d3e
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-03-06 02:30:35 +0100

    fix: generate secret key if none is found in env

--- src/slash_idk/settings.py
+++ src/slash_idk/settings.py
@@ -21,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY = config("SECRET_KEY")
+from decouple import config, UndefinedValueError
+try:
+    SECRET_KEY = config("SECRET_KEY")
+except UndefinedValueError:
+    from django.core.management.utils import get_random_secret_key  
+    SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
